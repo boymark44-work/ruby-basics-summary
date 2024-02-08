698dc5893f9ef7ce9c9dc28aaf9bfdca3484867a
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2024-02-08 17:06:40 +0800

    practice-coding: The gets method in Ruby and how to perform it using the terminal sice code-runner wont work.

---
+++ 04 - Object Methods/gets_method.rb
@@ -0,0 +1,12 @@
+# The gets method in Ruby
+puts "\nThe gets method in Ruby: "
+
+print "Hi, what's your name? "
+name = gets.chomp
+
+puts "Oh, so your name is #{name}? Nice to meet you!"
+
+print "Great, now, what's your age? "
+age = gets.chomp
+
+puts "Nice, so your name is #{name} and you are #{age} years old. Awesome, welcome to the team!"
