39c4304e9a23658e860f71a7325cc58c5099c56a
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2024-02-19 16:54:23 +0800

    practice-coding: Arrays in Ruby Part II along with the coding exercises.

---
+++ 14 - Arrays II/coding_exercise_custom_max.rb
@@ -0,0 +1,73 @@
+# A great way to grow your Ruby skills is to implement methods
+# built into the language. Of course, the goal is to NOT use the
+# original method in your solution; instead, come up with an alternate
+# implementation using other methods/approaches.
+#
+# Define a custom_max method that accepts an array.
+# The method should return the largest value in the array.
+# If the array is empty, the method should return nil.
+# Do not use the max method in your solution!
+#
+# Examples:
+# The => indicates the expected return value
+# custom_max([434.12, 723.99, 84.12, 649.92])  => 723.99
+# custom_max([8, 10, 2, 1, 19, 4])             => 19
+# custom_max(["Tree", "Elm", "Zebra"])         => "Zebra"
+# custom_max([])                               => nil
+
+
+# My own solution using the built-in max method:
+def custom_max(array)
+  if array.empty?
+    nil
+  else
+    array.max
+  end
+end
+
+p custom_max([434.12, 723.99, 84.12, 649.92]) # => 723.99
+p custom_max([8, 10, 2, 1, 19, 4]) # => 19
+p custom_max(["Tree", "Elm", "Zebra"]) # => "Zebra"
+p custom_max([]) # => nil
+puts
+
+
+# Using a custom max method:
+def custom_max(array)
+  # Base Case: Return nil if array.empty?
+  nil if array.empty?
+
+  largest_element = array[0]
+
+  array.each do |element|
+    largest_element = element if element > largest_element
+  end
+
+  largest_element
+end
+
+p custom_max([434.12, 723.99, 84.12, 649.92]) # => 723.99
+p custom_max([8, 10, 2, 1, 19, 4]) # => 19
+p custom_max(["Tree", "Elm", "Zebra"]) # => "Zebra"
+p custom_max([]) # => nil
+puts
+
+
+
+# Instructor solution:
+def custom_max(elements)
+  nil if elements.length == 0
+
+  max = elements[0]
+
+  elements.each do |element|
+    max = element if element > max
+  end
+
+  max
+end
+
+p custom_max([434.12, 723.99, 84.12, 649.92]) # => 723.99
+p custom_max([8, 10, 2, 1, 19, 4]) # => 19
+p custom_max(["Tree", "Elm", "Zebra"]) # => "Zebra"
+p custom_max([]) # => nil
